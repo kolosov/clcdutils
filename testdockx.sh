adfbd58e93d0b767548d350e02570638097df15f
Sergey Kolosov <kolosov@gmail.com>
2016-03-21 13:08:26 -0400

    Remove unnecessary -t docker option

--- testdockx.sh
+++ testdockx.sh
@@ -3,4 +3,4 @@ cd /home/liveuser
 cp /home/Dockerfile .
 
 docker build --rm -t xclock1 -f Dockerfile .
-docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix xclock1
+docker run -i --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix xclock1
