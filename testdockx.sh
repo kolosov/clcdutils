ef923434b791d3fd2cee7980761b1e9d84ea352f
Sergey Kolosov <kolosov@gmail.com>
2016-03-21 05:32:55 -0400

    Add startall.sh

--- testdockx.sh
+++ testdockx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /home/liveuser
+cp /home/Dockerfile .
 
-docker build --rm -t xclock1 -f /home/Dockerfile .
+docker build --rm -t xclock1 -f Dockerfile .
 docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix xclock1
